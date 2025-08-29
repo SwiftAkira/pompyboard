3316945c47bfcbf3ab6cefaeac12677c2077c833
pompydev <pompydev@proton.me>
2025-08-29 12:38:34 +0900

    web: add "coming soon" to product page

--- web/src/app/shop/[product]/page.tsx
+++ web/src/app/shop/[product]/page.tsx
@@ -1,8 +1,21 @@
-export default async function Page({
-    params,
-}: {
+import Link from "next/link"
+
+export default async function Page({}: {
     params: Promise<{ product: string }>
 }) {
-    const { product } = await params
-    return <div className="flex flex-col items-center gap-2 p-4">{product}</div>
+    return (
+        <div className="flex flex-col items-center gap-2 p-4">
+            <h2 className="text-6xl font-black">COMING SOON</h2>
+            <span>
+                Join our{" "}
+                <Link
+                    className="underline hover:underline-offset-2"
+                    href="https://discord.gg/h27rwcBn73"
+                >
+                    Discord
+                </Link>{" "}
+                server to get notified when the product launches
+            </span>
+        </div>
+    )
 }
