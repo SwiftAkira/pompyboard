09d1aa8f436ce97d6768aea199868820b0886ea4
pompydev <pompydev@proton.me>
2025-08-29 10:02:31 +0900

    web: link navbar title to home page

--- web/src/components/navbar.tsx
+++ web/src/components/navbar.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export default function Navbar() {
     return (
         <nav className="bg-white border-b border-zinc-200 h-16">
             <div className="flex items-center justify-between h-full mx-auto max-w-4xl">
-                <div className="flex items-center h-full">
+                <div className="flex px-2 relative items-center h-full">
                     <Image
                         className="aspect-square w-12 h-12 mr-4 select-none"
                         src="/logo.png"
@@ -12,9 +13,18 @@ export default function Navbar() {
                         height={48}
                         alt="pompyboard logo"
                     />
-                    <span className="font-extrabold text-lg select-none">
-                        Pompyboard
-                    </span>
+                    <Link
+                        href="/"
+                        aria-label="Go to home"
+                        className="before:absolute before:top-0 before:right-0 before:bottom-0 before:left-0"
+                    >
+                        <span
+                            aria-hidden
+                            className="font-extrabold text-lg select-none"
+                        >
+                            Pompyboard
+                        </span>
+                    </Link>
                 </div>
                 <div className="flex items-center h-full"></div>
             </div>
